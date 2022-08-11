aae88a20c406ee5b15fe9e2fc692128c4236b10c
PC8923 <jihanbhuiyan2020@gmail.com>
2022-08-10 20:46:09 -0400

    replace with clean data

---
+++ clean-data.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+ec <- read_csv("social_capital_county.csv") |>
+  select(county, county_name, pop2018, ec_county) |>
+  drop_na() |>
+  mutate(state = str_remove(county_name, ".*, "))
+
+write_rds(ec, "superior-ec.rds")
