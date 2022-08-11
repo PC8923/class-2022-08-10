3c13d6aaaf83d55fe42897ecc6c750a0cc4f2921
PC8923 <jihanbhuiyan2020@gmail.com>
2022-08-10 21:01:39 -0400

    move more to clean-data.R

--- clean-data.R
+++ clean-data.R
@@ -3,6 +3,8 @@ library(tidyverse)
 ec <- read_csv("social_capital_county.csv") |>
   select(county, county_name, pop2018, ec_county) |>
   drop_na() |>
-  mutate(state = str_remove(county_name, ".*, "))
+  mutate(state = str_remove(county_name, ".*, ")) |>
+  group_by(state) |> 
+  summarise(pop_state = sum(pop2018), avg_ec = mean(ec_county))
 
 write_rds(ec, "superior-ec.rds")
